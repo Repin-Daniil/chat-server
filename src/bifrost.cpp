6938f870b6f0674e05ea97c10775a05147077d10
DaniilR <daniil.r4@yandex.ru>
2024-11-01 14:02:31 +0500

    Fix bug with early producer destruction

--- src/bifrost.cpp
+++ src/bifrost.cpp
@@ -33,6 +33,7 @@ void ResetMetric(Stats& stats) {
 Bifrost::Bifrost(const components::ComponentConfig& config, const components::ComponentContext& context)
     : TcpAcceptorBase(config, context),
       stats_(context.FindComponent<components::StatisticsStorage>().GetMetricsStorage()->GetMetric(kTcpEchoTag)) {
+
 }
 
 
@@ -85,20 +86,25 @@ void Bifrost::ProcessSocket(engine::io::Socket&& sock) {
     }};
 
     if (mutex_.try_lock()) {
-      queue_1_ = Queue::Create();
+	  queue_1_ = Queue::Create();
       queue_2_ = Queue::Create();
-
-      if (!queue_1_->GetProducer().Push("AWAIT\r\n\r\n") ||
-      !queue_2_->GetProducer().Push("SEND\r\n\r\n")) {
-        return;
-      }
 
       auto send_task = utils::Async("send", DoSend, std::ref(sock), queue_2_->GetConsumer());
       DoRecv(sock, queue_1_->GetProducer(), stats_);
+
+//      queue_1_.reset();
+//      queue_2_.reset();
+
       mutex_.unlock();
     } else {
+	  auto producer = queue_2_->GetProducer();
+
+      if(!producer.Push("SEND\r\n\r\n")){
+        return;
+      }
+
       auto send_task = utils::Async("send", DoSend, std::ref(sock), queue_1_->GetConsumer());
-      DoRecv(sock, queue_2_->GetProducer(), stats_);
+      DoRecv(sock, std::move(producer), stats_);
     }
 
 
