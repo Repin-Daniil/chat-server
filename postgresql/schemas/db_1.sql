4c735b560c3d700a8f03186bdefa4bf3bd4598ac
Repin Daniil <daniil.r4@yandex.ru>
2025-03-19 18:04:08 +0500

    Improve schema

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -2,14 +2,18 @@ DROP SCHEMA IF EXISTS bifrost CASCADE;
 
 CREATE SCHEMA IF NOT EXISTS bifrost;
 
-CREATE EXTENSION pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto SCHEMA bifrost;
 
 CREATE TABLE IF NOT EXISTS bifrost.users (
-    id SERIAL PRIMARY KEY,
-    login varchar(100),
-    password varchar(100),
-    salt text,
-    registered timestamp DEFAULT(now())
+                                             id         SERIAL PRIMARY KEY,
+                                             login      TEXT        NOT NULL
+                                                 CONSTRAINT chk_login_length CHECK (LENGTH(login) BETWEEN 1 AND 100),
+                                             password   TEXT        NOT NULL
+                                                 CONSTRAINT chk_password_length CHECK (LENGTH(password) BETWEEN 8 AND 100),
+                                             salt       TEXT        NOT NULL,
+                                             registered TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE INDEX user_login_idx on bifrost.users using HASH(login);
+ALTER TABLE bifrost.users ADD CONSTRAINT unique_login UNIQUE (login);
+
+CREATE UNIQUE INDEX IF NOT EXISTS user_login_idx ON bifrost.users (login);
